3e55acee171be7cba6baefcc3f11cee09c344451
Jack Johannesen <jack@insertdomain.name>
2021-08-10 15:20:36 -0400

    fix: make completion cache to XDG_CACHE_HOME

--- bloat/completion.sh
+++ bloat/completion.sh
@@ -1,7 +1,11 @@
 #!/bin/zsh
 
-autoload bashcompinit; bashcompinit;
-autoload compinit; compinit;
+[[ -z "${XDG_CACHE_HOME}" ]] && XDG_CACHE_HOME="${HOME}/.cache";
+
+[[ ! -e "${XDG_CACHE_HOME}/zsh" ]] && mkdir -p "${XDG_CACHE_HOME}/zsh";
+
+autoload bashcompinit; bashcompinit -d "${XDG_CACHE_HOME}/zsh/bashcompdump";
+autoload compinit; compinit -d "${XDG_CACHE_HOME}/zsh/zcompdump";
 
 zstyle ':completion:*:*:*:*:*' menu select;
 zstyle ':completion:*' list-colors;
